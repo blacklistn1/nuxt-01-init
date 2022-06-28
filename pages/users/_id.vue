5b754e00d541a354aa556031825368433886f633
Kevin Tran <kevin.tran@vmodev.com>
2022-06-28 11:36:44 +0700

    Basics of routing and pages folder structure

---
+++ pages/users/_id.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3 class="text-3xl">Route param is: {{ $route.params.id }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+
+  mounted() {
+    console.log(this.$route.query);
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/users/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1 class="text-5xl font-bold">User page: {{ msg }}</h1>
+    <input type="text" v-model="id" class="border border-gray-300 text-lg p-2">
+    <button @click="loadUser" class="p-3 bg-gray-300">Load user</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello',
+      id: '',
+    }
+  },
+  methods: {
+    loadUser() {
+      this.$router.push('/users/' + this.id)
+    }
+  }
+}
+</script>
+
+<style scoped lang="postcss">
+
+</style>
